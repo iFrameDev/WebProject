bbdac4db78c4acc88049496e35ad880790e441dc
iFrame <iframedev@gmail.com>
2022-11-24 19:19:07 +0100

    update

--- API/server.js
+++ API/server.js
@@ -32,13 +32,13 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to application." });
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 
-require("./src/routes/tutorial.routes")(app);
+require("./src/routes/tutorial.routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
---
+++ API/src/pages/home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div className="home">
+            <h1>Test</h1>
+            <div>bienvenue chez lena</div>
+        </div>
+    )
+}
